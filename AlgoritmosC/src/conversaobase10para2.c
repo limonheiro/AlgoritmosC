f5a1d184abc1e5829b820363102966e03fbca2ad
jppreti <jppreti@gmail.com>
2016-05-31 11:34:41 -0400

    Exemplo de Conversao de base10 para base2 com uso de pilha.

---
+++ AlgoritmosC/src/conversaobase10para2.c
@@ -0,0 +1,30 @@
+/**************************************************************************
+ * Programa conversaobase10para2.c	                                      *
+ *                                                                        *
+ * Demonstracão do uso de pilha na conversão de base					  *
+ * Data: 30/05/2016			                                              *
+ * Autor: João Paulo D. Preti                                             *
+ **************************************************************************/
+/*
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "iStack.h"
+
+int main() {
+	int base10 = 34;
+	iStack pilha;
+	init(&pilha);
+	int resultado=0;
+	do {
+		resultado = base10/2;
+		int resto = base10%2;
+		push(&pilha,resto);
+		base10=resultado;
+	} while (resultado>0);
+
+	show(&pilha);
+
+	return EXIT_SUCCESS;
+}
+*/
