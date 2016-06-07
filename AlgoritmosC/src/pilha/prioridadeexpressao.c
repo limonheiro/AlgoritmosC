46567f773a1c59f76ddac508e00f75589d476deb
jppreti <jppreti@gmail.com>
2016-06-07 10:30:08 -0400

    Modulizado o algoritmo de prioridadeexpressao.

--- AlgoritmosC/src/pilha/prioridadeexpressao.c
+++ AlgoritmosC/src/pilha/prioridadeexpressao.c
@@ -4,7 +4,7 @@
  * Verificando a corretude na prioridade das expressões com pilha		  *
  * Autor: João Paulo D. Preti                                             *
  **************************************************************************/
-/*
+/*d
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
