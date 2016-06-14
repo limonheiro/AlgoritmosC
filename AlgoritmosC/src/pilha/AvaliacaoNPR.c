0069f110756ddaa81bcb9053dc0e1f5a987cec85
jppreti <jppreti@gmail.com>
2016-06-14 14:06:29 -0400

    Programa para avaliar expressão em NPR.

---
+++ AlgoritmosC/src/pilha/AvaliacaoNPR.c
@@ -0,0 +1,15 @@
+/*
+ * AvaliacaoNPR.c
+ *
+ *  Created on: 9 de jun de 2016
+ *      Author: joaopaulodelgadopreti
+ */
+
+#include "npr.h"
+
+int main() {
+	char sentence[] = "2 3 + 5 *";
+	printf("%f", avalia(sentence));
+	return 0;
+}
+
